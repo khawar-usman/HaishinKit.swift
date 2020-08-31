b4cb90bfb337715b8add96f7ac0dc3fed99e22c2
Usman Khawar <kh.usman.khawar@gmail.com>
2020-09-01 01:33:49 +0500

    removed special characters from file

--- Sources/Util/FilenameUtil.swift
+++ Sources/Util/FilenameUtil.swift
@@ -11,6 +11,14 @@ struct FilenameUtil {
         if let value = result.split(separator: "/").last {
             result = String(value)
         }
+        var invalidCharacters = CharacterSet(charactersIn: ":/")
+        invalidCharacters.formUnion(.newlines)
+        invalidCharacters.formUnion(.illegalCharacters)
+        invalidCharacters.formUnion(.controlCharacters)
+        result = result
+            .components(separatedBy: invalidCharacters)
+            .joined(separator: "")
+
         if result.count < Int(FILENAME_MAX) {
             return result
         }
